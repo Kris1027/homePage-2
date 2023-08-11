f6db325bffd21a3db121b146513101954ccf9792
Kris1027 <obarzanek.work@gmail.com>
2023-08-11 13:35:26 +0200

    add font-awsome

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,17 @@
 import styles from './Homepage.module.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fas);
+
 function Homepage() {
-  return <div className={styles.websitesContainer}>elo</div>;
+  return (
+    <div className={styles.websitesContainer}>
+      <FontAwesomeIcon icon='coffee' />
+    </div>
+  );
 }
 
 export default Homepage;
