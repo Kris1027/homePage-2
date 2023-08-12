1e0eb7f5b2961053829e4d9715400a2e1d6fb24b
Kris1027 <obarzanek.work@gmail.com>
2023-08-12 23:25:41 +0200

    add links to my game and english teacher websites

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -29,6 +29,26 @@ function NavPage() {
             <span>NotePad</span>
           </NavLink>
         </li>
+        <li>
+          <a
+            href='https://kris1027.github.io/Frela---The-Game/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon='fa-gamepad' />
+            <span>Frela - The Game</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://delicate-tiramisu-04ae2c.netlify.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FontAwesomeIcon icon='fa-graduation-cap' />
+            <span>myEnglishTeacher</span>
+          </a>
+        </li>
       </ul>
     </nav>
   );
