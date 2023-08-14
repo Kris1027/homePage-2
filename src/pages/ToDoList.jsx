ff1422100ddefeab633a9d503797f8413ebc692f
Kris1027 <obarzanek.work@gmail.com>
2023-08-14 11:10:37 +0200

    start with todoapp

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,11 +1,35 @@
 import NavPage from '../components/NavPage';
 import styles from './ToDoList.module.css';
 
+const lists = [
+  {
+    task: 'kupić marchewkę',
+    isCompleted: true,
+  },
+  {
+    task: 'ogolić wąsa',
+    isCompleted: false,
+  },
+];
+
 function ToDoList() {
   return (
     <>
       <NavPage />
-      <div className={styles.toDoList}></div>
+      <div className={styles.toDoWrapper}>
+        <h1>ToDo App</h1>
+        <form>
+          <input type='text' />
+          <button>+</button>
+        </form>
+        <div className='tasks'>
+          {lists.map((task) => (
+            <div key={task.task}>{task.task}</div>
+          ))}
+        </div>
+        <p>You have X pending tasks</p>
+        <button>Clear All</button>
+      </div>
     </>
   );
 }
